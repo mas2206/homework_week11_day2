d98657dab1792bcac030254ccc283ea62e3c0492
Michael Shennan <m.shennan22@gmail.com>
2017-05-16 21:57:08 +0100

    added 'take me to australia' button

--- public/app.js
+++ public/app.js
@@ -1,12 +1,23 @@
 var initialize = function() {
+
   var mapDiv = document.getElementById("main-map");
   var center = {lat: 55.86, lng: -4.25};
   var mainMap = new MapWrapper(mapDiv, center, 5);
+
   mainMap.addMarker({lat: 55.95, lng: -3.18}, "Edinburgh - capital city of Scotland");
   mainMap.addMarker({lat: 51.50, lng: -0.12}, "London - capital city of England");
   mainMap.addMarker({lat: 51.48, lng: -3.17}, "Cardiff - capital city of Wales");
   mainMap.addMarker({lat: 54.59, lng: -5.93}, "Belfast - capital city of Northern Ireland");
   mainMap.addMarker({lat: 53.34, lng: -6.26}, "Dublin - capital city of the Republic of Ireland");
-}
+
+  var ausButton = document.getElementById("aus-button");
+
+  var handleButtonClick = function(){
+    mainMap.googleMap.setCenter({lat: -25.27, lng: 133.77});
+    mainMap.googleMap.setZoom(4);
+  }
+  ausButton.addEventListener('click', handleButtonClick);
+
+};
 
 window.addEventListener("load", initialize);
