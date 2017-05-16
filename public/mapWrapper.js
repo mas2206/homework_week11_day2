a19550c356e9384174ee9f6c41aa071c8408b69c
Michael Shennan <m.shennan22@gmail.com>
2017-05-16 19:58:32 +0100

    added mapWrapper

---
+++ public/mapWrapper.js
@@ -0,0 +1,24 @@
+var MapWrapper = function(container, coords, zoom) {
+  this.googleMap = new google.maps.Map(container, {
+    center: coords,
+    zoom: zoom
+  });
+}
+
+MapWrapper.prototype = {
+
+  addMarker: function(coords) {
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: this.googleMap
+    });
+  },
+
+  addClickEvent: function() {
+    google.maps.event.addListener(this.googleMap, "click", function(event) {
+      var position = {lat: event.latLng.lat(), lng: event.latLng.lng()};
+      this.addMarker(position);
+    }.bind(this));
+  }
+
+}
